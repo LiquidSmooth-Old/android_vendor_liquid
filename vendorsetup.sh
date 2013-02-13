54d244869689a006f3fcac279b03c8adc7f418d7
Sean Hoyt <seanhoyt963@gmail.com>
2013-02-12 20:45:16 -0600

    Removed 2 devices untill fully finished
    glacier and ace

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,9 @@
-add_lunch_combo liquid_ace-userdebug
 add_lunch_combo liquid_d2att-userdebug
 add_lunch_combo liquid_d2mtr-userdebug
 add_lunch_combo liquid_d2spr-userdebug
 add_lunch_combo liquid_d2tmo-userdebug
 add_lunch_combo liquid_d2usc-userdebug
 add_lunch_combo liquid_d2vzw-userdebug
-add_lunch_combo liquid_glacier-userdebug
 add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_i605-userdebug
 add_lunch_combo liquid_i9300-userdebug
