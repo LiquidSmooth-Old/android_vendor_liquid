f16cc47e8d302aebdc7c6f36e39c8b6e6531949f
liquid0624 <rjbirnesser@gmail.com>
2013-03-11 03:16:23 -0400

    Oops Forgot Two Lunch Items

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ add_lunch_combo liquid_d2spr-userdebug
 add_lunch_combo liquid_d2tmo-userdebug
 add_lunch_combo liquid_d2usc-userdebug
 add_lunch_combo liquid_d2vzw-userdebug
+add_lunch_combo liquid_evita-userdebug
 add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_i605-userdebug
 add_lunch_combo liquid_i9300-userdebug
@@ -23,3 +24,4 @@ add_lunch_combo liquid_tilapia-userdebug
 add_lunch_combo liquid_toro-userdebug
 add_lunch_combo liquid_toroplus-userdebug
 add_lunch_combo liquid_umts_spyder-userdebug
+add_lunch_combo liquid_ville-userdebug
