d50a26d5557596477e6b3647bec61cd69f967dcf
Sean Hoyt <seanhoyt963@gmail.com>
2013-04-29 21:10:02 -0500

    added HTC Glacier

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ add_lunch_combo liquid_d2spr-userdebug
 add_lunch_combo liquid_d2tmo-userdebug
 add_lunch_combo liquid_d2usc-userdebug
 add_lunch_combo liquid_d2vzw-userdebug
+add_lunch_combo liquid_glacier-userdebug
 add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_i605-userdebug
 add_lunch_combo liquid_i9100-userdebug
