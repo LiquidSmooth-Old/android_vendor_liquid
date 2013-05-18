97b1f108f9891e2c3754678b1715f93c8c58965b
liquid0624 <rjbirnesser@gmail.com>
2013-05-18 13:19:31 -0400

    Update Liquid Lunch Menu Items

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,14 +4,12 @@ add_lunch_combo liquid_d2spr-userdebug
 add_lunch_combo liquid_d2tmo-userdebug
 add_lunch_combo liquid_d2usc-userdebug
 add_lunch_combo liquid_d2vzw-userdebug
-add_lunch_combo liquid_glacier-userdebug
 add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_i605-userdebug
 add_lunch_combo liquid_i9100-userdebug
 add_lunch_combo liquid_i9300-userdebug
 add_lunch_combo liquid_i9305-userdebug
 add_lunch_combo liquid_l900-userdebug
-add_lunch_combo liquid_infuse4g-userdebug
 add_lunch_combo liquid_maguro-userdebug
 add_lunch_combo liquid_mako-userdebug
 add_lunch_combo liquid_manta-userdebug
