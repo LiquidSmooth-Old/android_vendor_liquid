b55b4ad6fa49c546afa7bd3ae01d4c827919a737
liquid0624 <rjbirnesser@gmail.com>
2013-07-20 13:20:58 -0400

    Enable release flag/fixed motorola boot animation

--- config/common.mk
+++ config/common.mk
@@ -118,7 +118,7 @@ PRODUCT_COPY_FILES += \
     vendor/liquid/config/permissions/com.liquidsmooth.nfc.enhanced.xml:system/etc/permissions/com.liquidsmooth.nfc.enhanced.xml
 
 # version
-RELEASE = false
+RELEASE = true
 LIQUID_VERSION_MAJOR = 2
 LIQUID_VERSION_MINOR = 9
 
