b0d96fa5d5f76233eb37302ce0a49d2ffb47ab06
Sean Hoyt <seanhoyt963@gmail.com>
2013-10-28 08:09:10 -0500

    Add nfc enchanced

---
+++ config/nfc_enhanced.mk
@@ -0,0 +1,3 @@
+# This device supports Liquid enhanced NFC
+PRODUCT_COPY_FILES += \
+    vendor/cm/config/permissions/com.liquidsmooth.nfc.enhanced.xml:system/etc/permissions/com.liquidsmooth.nfc.enhanced.xml
