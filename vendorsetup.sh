b066983915b9f49f910b4524d18845e84f582209
Sean Hoyt <seanhoyt963@gmail.com>
2013-12-28 20:40:25 -0600

    Add Note 3 vzw

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,6 +13,7 @@ add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_hammerhead-userdebug
 add_lunch_combo liquid_hltespr-userdebug
 add_lunch_combo liquid_hltetmo-userdebug
+add_lunch_combo liquid_hltevzw-userdebug
 add_lunch_combo liquid_hltexx-userdebug
 add_lunch_combo liquid_i605-userdebug
 add_lunch_combo liquid_i9300-userdebug
