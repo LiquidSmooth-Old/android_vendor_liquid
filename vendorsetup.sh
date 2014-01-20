7301f4fcce44a87a400e0b1677bee2d4ae86a1c2
Sean Hoyt <seanhoyt963@gmail.com>
2014-01-20 00:24:21 -0600

    Add meliusltexx
    Samsung Galaxy Mega (LTE model)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -32,6 +32,7 @@ add_lunch_combo liquid_mako-userdebug
 add_lunch_combo liquid_manta-userdebug
 add_lunch_combo liquid_maserati-userdebug
 add_lunch_combo liquid_mb886-userdebug
+add_lunch_combo liquid_meliusltexx-userdebug
 add_lunch_combo liquid_m7spr-userdebug
 add_lunch_combo liquid_m7-userdebug
 add_lunch_combo liquid_m7vzw-userdebug
