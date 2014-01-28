SlimRoms/vendor_slim

Enable gesture typing - (2/3) - add prebuilt lib
Change-Id: I82dfe7f6cf48f98b3fd03d3775c5231b0d4e3b79

config/common.mk
@@ -34,6 +34,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/etc/init.local.rc:root/init.slim.rc
 
+# Copy latinime for gesture typing
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+
 # Compcache/Zram support
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/bin/compcache:system/bin/compcache \

LiquidSmooth-Old/android_vendor_liquid

Conflicts:
	config/common.mk

config/common.mk
@@ -51,6 +51,10 @@ PRODUCT_PACKAGES += \
 # superuser
 SUPERUSER_EMBEDDED := true
 
+# Copy latinime for gesture typing
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+
 PRODUCT_PACKAGES += \
     Superuser \
     su