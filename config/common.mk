048983f4134eb6bdac11ef2416bf384e7c1b6a1a
Jason H Weitzel <jsnweitzel@gmail.com>
2014-02-07 18:44:29 -0500

    Tether for ALL 	modified:   config/common.mk

--- config/common.mk
+++ config/common.mk
@@ -18,6 +18,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1 \
     persist.sys.root_access=3
 
+# Tether for all
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
+
 # others
 PRODUCT_PACKAGES += \
     Apollo \
