51145149ba30dec2f7a4cb3489a92f51281f06cc
Sean Hoyt <seanhoyt963@gmail.com>
2014-02-16 04:10:23 -0600

    add shooteru

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,6 +27,7 @@ add_lunch_combo liquid_m7-userdebug
 add_lunch_combo liquid_m7vzw-userdebug
 add_lunch_combo liquid_n7100-userdebug
 add_lunch_combo liquid_odin-userdebug
+add_lunch_combo liquid_shooteru-userdebug
 add_lunch_combo liquid_solana-userdebug
 add_lunch_combo liquid_spyder-userdebug
 add_lunch_combo liquid_t0lte-userdebug
