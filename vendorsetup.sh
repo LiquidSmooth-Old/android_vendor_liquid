08c9e47e844062b12579c38c3b62848edaff6306
Sean Hoyt <seanhoyt963@gmail.com>
2014-03-05 16:10:27 -0600

    Remove soon to be unified devices and the note 3
    I have given up on the note 3, been trying to get it to work for months.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,16 +11,13 @@ add_lunch_combo liquid_flo-userdebug
 add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_hammerhead-userdebug
 add_lunch_combo liquid_hayabusa-userdebug
-add_lunch_combo liquid_hlte-userdebug
 add_lunch_combo liquid_honami-userdebug
 add_lunch_combo liquid_huashan-userdebug
-add_lunch_combo liquid_i605-userdebug
 add_lunch_combo liquid_i9300-userdebug
 add_lunch_combo liquid_i9305-userdebug
 add_lunch_combo liquid_i9500-userdebug
 add_lunch_combo liquid_jflte-userdebug
 add_lunch_combo liquid_ls980-userdebug
-add_lunch_combo liquid_l900-userdebug
 add_lunch_combo liquid_maguro-userdebug
 add_lunch_combo liquid_mako-userdebug
 add_lunch_combo liquid_manta-userdebug
@@ -32,7 +29,6 @@ add_lunch_combo liquid_m7-userdebug
 add_lunch_combo liquid_m7vzw-userdebug
 add_lunch_combo liquid_moto_msm8960-userdebug
 add_lunch_combo liquid_moto_msm8960dt-userdebug
-add_lunch_combo liquid_n7100-userdebug
 add_lunch_combo liquid_nicki-userdebug
 add_lunch_combo liquid_odin-userdebug
 add_lunch_combo liquid_pollux-userdebug
