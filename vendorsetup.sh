e95c38b75e1be69861851f5d8fb275636de4bccd
Brock Shahan <brock.shahan@cox.net>
2014-04-08 21:23:31 -0500

    Add M8

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -35,6 +35,9 @@ add_lunch_combo liquid_mint-userdebug
 add_lunch_combo liquid_m7spr-userdebug
 add_lunch_combo liquid_m7-userdebug
 add_lunch_combo liquid_m7vzw-userdebug
+add_lunch_combo liquid_m8spr-userdebug
+add_lunch_combo liquid_m8-userdebug
+add_lunch_combo liquid_m8vzw-userdebug
 add_lunch_combo liquid_moto_msm8960-userdebug
 add_lunch_combo liquid_moto_msm8960dt-userdebug
 add_lunch_combo liquid_n7100-userdebug
