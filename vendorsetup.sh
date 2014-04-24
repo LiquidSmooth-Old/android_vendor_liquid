6bf6bbf4f2fda891ce73eaeb278bdfa8a338f566
Sean Hoyt <seanhoyt963@gmail.com>
2014-04-24 08:22:15 -0500

    Remove Sony's they will not be ready before release

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
-add_lunch_combo liquid_amami-userdebug
 add_lunch_combo liquid_codina-userdebug
 add_lunch_combo liquid_d2lte-userdebug
 add_lunch_combo liquid_d800-userdebug
@@ -16,11 +15,9 @@ add_lunch_combo liquid_geehrc-userdebug
 add_lunch_combo liquid_geespr-userdebug
 add_lunch_combo liquid_grouper-userdebug
 add_lunch_combo liquid_hammerhead-userdebug
-add_lunch_combo liquid_hayabusa-userdebug
 add_lunch_combo liquid_hercules-userdebug
 add_lunch_combo liquid_honami-userdebug
 add_lunch_combo liquid_hlte-userdebug
-add_lunch_combo liquid_huashan-userdebug
 add_lunch_combo liquid_i605-userdebug
 add_lunch_combo liquid_i9300-userdebug
 add_lunch_combo liquid_i9305-userdebug
@@ -34,7 +31,6 @@ add_lunch_combo liquid_mako-userdebug
 add_lunch_combo liquid_manta-userdebug
 add_lunch_combo liquid_maserati-userdebug
 add_lunch_combo liquid_meliusltexx-userdebug
-add_lunch_combo liquid_mint-userdebug
 add_lunch_combo liquid_m7spr-userdebug
 add_lunch_combo liquid_m7-userdebug
 add_lunch_combo liquid_m7vzw-userdebug
@@ -44,12 +40,8 @@ add_lunch_combo liquid_m8vzw-userdebug
 add_lunch_combo liquid_moto_msm8960-userdebug
 add_lunch_combo liquid_moto_msm8960dt-userdebug
 add_lunch_combo liquid_n7100-userdebug
-add_lunch_combo liquid_nicki-userdebug
-add_lunch_combo liquid_odin-userdebug
 add_lunch_combo liquid_p3100-userdebug
 add_lunch_combo liquid_p3110-userdebug
-add_lunch_combo liquid_pollux-userdebug
-add_lunch_combo liquid_pollux_windy-userdebug
 add_lunch_combo liquid_serranoltexx-userdebug
 add_lunch_combo liquid_serrano3gxx-userdebug
 add_lunch_combo liquid_shooter-userdebug
@@ -58,12 +50,8 @@ add_lunch_combo liquid_spyder-userdebug
 add_lunch_combo liquid_skyrocket-userdebug
 add_lunch_combo liquid_t0lte-userdebug
 add_lunch_combo liquid_targa-userdebug
-add_lunch_combo liquid_tsubasa-userdebug
-add_lunch_combo liquid_togari_gpe-userdebug
-add_lunch_combo liquid_togari-userdebug
 add_lunch_combo liquid_toro-userdebug
 add_lunch_combo liquid_toroplus-userdebug
 add_lunch_combo liquid_umts_spyder-userdebug
 add_lunch_combo liquid_v500-userdebug
 add_lunch_combo liquid_vs980-userdebug
-add_lunch_combo liquid_yuga-userdebug
