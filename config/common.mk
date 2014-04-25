4f7e932dfc666f5711479dbf6fbd374839e3d05a
Sean Hoyt <seanhoyt963@gmail.com>
2014-04-25 08:33:04 -0500

    Add Inspire Launcher

--- config/common.mk
+++ config/common.mk
@@ -70,6 +70,10 @@ PRODUCT_COPY_FILES += \
 # telephony
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver
+	
+# Inspire Launcher
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/app/inspirelauncher.apk:system/app/inspirelauncher.apk \
 
 # themes
 PRODUCT_PACKAGES += \
