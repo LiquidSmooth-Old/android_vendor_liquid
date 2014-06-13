08eae7602e82d60cb54f15825db88f67833c0775
TheBr0ken <jtoro2716@gmail.com>
2014-06-13 15:54:34 -0500

    Enable better scrolling speeds

--- config/common.mk
+++ config/common.mk
@@ -194,6 +194,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     pm.sleep.mode=1 \
     wifi.supplicant_scan_interval=180 \
+    windowsmgr.max_events_per_sec=150 \
     net.tcp.buffersize.default=4096,87380,256960,4096,16384,256960 \
     net.tcp.buffersize.wifi=4096,87380,256960,4096,16384,256960 \
     net.tcp.buffersize.umts=4096,87380,256960,4096,16384,256960 \
