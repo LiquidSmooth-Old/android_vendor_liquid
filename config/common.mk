DirtyUnicorns/android_vendor_du

Added DU_HFM support
Change-Id: I19df2d8617bd9d10833348da59c20eeb9ac816e8

config/common.mk
@@ -80,6 +80,11 @@ PRODUCT_COPY_FILES += \
     external/koush/Superuser/init.superuser.rc:root/init.superuser.rc \
     vendor/du/proprietary/Term/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
     
+# HFM Files
+PRODUCT_COPY_FILES += \
+	vendor/du/prebuilt/etc/hosts.alt:system/etc/hosts.alt \
+	vendor/du/prebuilt/etc/hosts.og:system/etc/hosts.og
+
 #DUStats
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.romstats.url=http://stats.dirtyunicorns.com/ \

LiquidSmooth-Old/android_vendor_liquid

Conflicts:
	config/common.mk
	config/packages.mk

config/common.mk
@@ -179,6 +179,11 @@ else
     LIQUID_VERSION := LS-KK-v$(LIQUID_VERSION_MAJOR).$(LIQUID_VERSION_MINOR)-$(LIQUID_VERSION_STATE)
 endif
 
+# HFM Files
+PRODUCT_COPY_FILES += \
+	vendor/liquid/prebuilt/etc/hosts.alt:system/etc/hosts.alt \
+	vendor/liquid/prebuilt/etc/hosts.og:system/etc/hosts.og
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.liquid.version=$(LIQUID_VERSION)
 