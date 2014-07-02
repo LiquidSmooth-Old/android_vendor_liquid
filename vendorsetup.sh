4534b7b09f3a56b73207152cf12912002e75d4a3
Sean Hoyt <seanhoyt963@gmail.com>
2014-07-02 18:52:09 -0500

    Add d710

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,7 @@ add_lunch_combo liquid_apexqtmo-userdebug
 add_lunch_combo liquid_bacon-userdebug
 add_lunch_combo liquid_codina-userdebug
 add_lunch_combo liquid_d2lte-userdebug
+add_lunch_combo liquid_d710-userdebug
 add_lunch_combo liquid_d800-userdebug
 add_lunch_combo liquid_d801-userdebug
 add_lunch_combo liquid_d802-userdebug
