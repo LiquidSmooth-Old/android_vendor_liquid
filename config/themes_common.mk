Flamingo-OS/vendor_flamingo

Theme Engine [2/8]

configs/themes_common.mk
@@ -1,0 +1,8 @@
+# Theme engine
+PRODUCT_PACKAGES += \
+    ThemeChooser \
+    ThemesProvider
+
+PRODUCT_COPY_FILES += \
+    vendor/pa/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/pa/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml

LiquidSmooth-Old/android_vendor_liquid

config/themes_common.mk
@@ -1,8 +1,8 @@
-# T-Mobile theme engine
+# Theme engine
 PRODUCT_PACKAGES += \
-       ThemeManager \
        ThemeChooser \
-       com.tmobile.themes
+       ThemesProvider
 
 PRODUCT_COPY_FILES += \
-    vendor/liquid/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+    vendor/liquid/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+    vendor/liquid/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml