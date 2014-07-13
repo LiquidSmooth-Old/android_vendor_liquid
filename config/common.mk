fa1588af021ecc2675d063c187271e064e976649
Sean Hoyt <seanhoyt963@gmail.com>
2014-07-13 18:01:06 -0500

    unfreeze and bring on 3.2

--- config/common.mk
+++ config/common.mk
@@ -166,9 +166,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # version
-RELEASE = true
+RELEASE = false
 LIQUID_VERSION_MAJOR = 3
-LIQUID_VERSION_MINOR = 1
+LIQUID_VERSION_MINOR = 2
 
 # release
 ifeq ($(RELEASE),true)
