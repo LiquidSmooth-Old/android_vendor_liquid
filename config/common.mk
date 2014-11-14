15a3003edac566d1e3dd712eaff8724a31805a20
johnnyslt <ionut.darescu@gmail.com>
2014-11-14 08:56:50 -0500

    Revert "Switch to milestone instead of Official"
    This reverts commit 178bd992c5658c83dffb2f5ae96f3cdd576f00ba.
    
    Change-Id: I3e6a7095d3fdd56760297f369f12050488ced31b

--- config/common.mk
+++ config/common.mk
@@ -143,13 +143,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # version
-RELEASE = true
+RELEASE = false
 LIQUID_VERSION_MAJOR = 3
 LIQUID_VERSION_MINOR = 2
 
 # release
 ifeq ($(RELEASE),true)
-    LIQUID_VERSION := LS-KK-MileStone-$(LIQUID_VERSION_MAJOR).$(LIQUID_VERSION_MINOR)
+    LIQUID_VERSION_STATE := OFFICIAL
+    LIQUID_VERSION := LS-KK-v$(LIQUID_VERSION_MAJOR).$(LIQUID_VERSION_MINOR)-$(LIQUID_VERSION_STATE)
 else
     LIQUID_VERSION_STATE := $(shell date +%Y-%m-%d)
     LIQUID_VERSION := LS-KK-v$(LIQUID_VERSION_MAJOR).$(LIQUID_VERSION_MINOR)-$(LIQUID_VERSION_STATE)
