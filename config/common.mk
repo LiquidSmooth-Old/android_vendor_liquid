a7e7815cd8f229848c4656d0718f23515c0e5bc8
deadman96385 <seanhoyt963@gmail.com>
2015-02-11 01:47:45 -0500

    Cleanup permissions
    Change-Id: I43e0d7fb2f2de065e249cc5756ffbeda9da58842

--- config/common.mk
+++ config/common.mk
@@ -59,11 +59,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Tether for all
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
-ifneq ($(TARGET_BUILD_VARIANT),eng)
-# Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
-endif
-
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
@@ -105,10 +100,6 @@ PRODUCT_COPY_FILES += \
 # Enable wireless Xbox 360 controller support
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
-
-# This is liquid!
-PRODUCT_COPY_FILES += \
-    vendor/liquid/config/permissions/com.liquidsmooth.android.xml:system/etc/permissions/com.liquidsmooth.android.xml
 
 # T-Mobile theme engine
 include vendor/liquid/config/themes_common.mk
