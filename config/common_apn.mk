1f4ece16d2e75aaa2ddfd6e5111acd1b1e2e9d46
Ricardo Cerqueira <ricardo@cyngn.com>
2015-04-27 15:06:49 +0100

    Kill spn-conf overrides
    These were overloading the ASCII names to region-specific charsets,
    which looks nice but it's horribly confusing if you're not native
    from those regions and happen to be roaming there.
    
    So... like our upstreams, just show the original carrier names
    
    Change-Id: Icb3c6c9be2a4ee57866f2f57411a4f441be88f7c

--- config/common_apn.mk
+++ config/common_apn.mk
@@ -1,6 +1,6 @@
 # World APN list
 PRODUCT_COPY_FILES += \
-    vendor/lidqui/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/liquid/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
