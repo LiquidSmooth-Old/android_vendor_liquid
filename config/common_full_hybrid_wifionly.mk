e9a5bb717ed163a58d52e3be7541965b5f4f06f5
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-05-27 01:27:08 -0400

    Reconfigure config files and add Common TV
    Change-Id: Ie29548b7160ee24331d9833ff769acc82ff10e7b
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- config/common_full_hybrid_wifionly.mk
+++
@@ -1,8 +0,0 @@
-# Inherit common stuff
-$(call inherit-product, vendor/liquid/config/common.mk)
-
-PRODUCT_PACKAGE_OVERLAYS += vendor/liquid/overlay/tablet
-
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
---
+++ config/common_tv.mk
@@ -0,0 +1,7 @@
+# Inherit common stuff
+$(call inherit-product, vendor/liquid/config/common.mk)
+
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Helium.ogg
