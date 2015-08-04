kn8wolf/vendor_slim

Change how we change the lcd density
This allows us to edit the props without re-zipping

Change-Id: I6d59ecf79f765009e57deabccd5de074b7a7d006

config/common.mk
@@ -179,3 +179,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=$(SLIM_MOD_VERSION) \
     ro.slim.buildtype=$(SLIM_BUILD_TYPE)
 
+EXTENDED_POST_PROCESS_PROPS := vendor/slim/tools/slim_process_props.py
+

LiquidSmooth-Old/android_vendor_liquid

config/common.mk
@@ -190,3 +190,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ifneq ($(TARGET_ENABLE_UKM),true)
 -include vendor/liquid/config/common_ukm.mk
 endif
+
+EXTENDED_POST_PROCESS_PROPS := vendor/liquid/tools/slim_process_props.py
+