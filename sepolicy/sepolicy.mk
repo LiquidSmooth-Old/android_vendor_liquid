9fa19b9b18b47ea2ca836ec92135e7d36780806a
Jason H Weitzel <jsnweitzel@gmail.com>
2015-10-12 07:58:27 -0400

    updates for mm 	modified:   config/common.mk 	modified:   prebuilt/common/bin/backuptool.functions 	modified:   prebuilt/common/bin/backuptool.sh 	modified:   prebuilt/common/etc/init.local.rc 	modified:   sepolicy/file.te 	modified:   sepolicy/file_contexts 	modified:   sepolicy/property_contexts 	new file:   sepolicy/qcom/bootanim.te 	new file:   sepolicy/qcom/mpdecision.te 	new file:   sepolicy/qcom/perfd.te 	new file:   sepolicy/qcom/sepolicy.mk 	new file:   sepolicy/qcom/system_server.te 	new file:   sepolicy/qcom/vold.te 	modified:   sepolicy/recovery.te 	modified:   sepolicy/sepolicy.mk 	modified:   sepolicy/system.te 	modified:   sepolicy/system_server.te
    Change-Id: I36313ed67ee3d456c443261b145ab0ba8aed0ef6

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,33 +1,8 @@
 #
 # This policy configuration will be used by all products that
-# inherit from CM
+# inherit from LS
 #
 
 BOARD_SEPOLICY_DIRS += \
     vendor/liquid/sepolicy
 
-BOARD_SEPOLICY_UNION += \
-    file.te \
-    file_contexts \
-    genfs_contexts \
-    property_contexts \
-    seapp_contexts \
-    service_contexts \
-    auditd.te \
-    adbd.te \
-    healthd.te \
-    hostapd.te \
-    installd.te \
-    netd.te \
-    property.te \
-    recovery.te \
-    shell.te \
-    sysinit.te \
-    system.te \
-    system_app.te \
-    system_server.te \
-    ueventd.te \
-    uncrypt.te \
-    userinit.te \
-    vold.te \
-    mac_permissions.xml
